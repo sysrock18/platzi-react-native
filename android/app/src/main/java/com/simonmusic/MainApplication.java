8392715e1d8b42040685bda0cd0d18162fd6c68f
Simon <gonzalezsimon18@gmail.com>
2017-12-09 18:26:53 -0300

    Integrar firebase con el login de facebook

--- android/app/src/main/java/com/simonmusic/MainApplication.java
+++ android/app/src/main/java/com/simonmusic/MainApplication.java
@@ -3,6 +3,7 @@ package com.simonmusic;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -42,6 +43,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
           new FBSDKPackage(mCallbackManager)
       );
     }
--- src/Login.js
+++ src/Login.js
@@ -2,31 +2,66 @@ import React, { Component } from 'react';
 import {
   View,
   StyleSheet,
-  Text
+  Text,
+  Button
 } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import FBSDK, { LoginButton, AccessToken } from 'react-native-fbsdk';
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+const config = {
+  apiKey: 'AIzaSyDvBMfM74mwQXoJpEwTWn-la8HAf39AuzI',
+  authDomain: 'simonmusic-1d3ae.firebaseapp.com',
+  databaseURL: 'https://simonmusic-1d3ae.firebaseio.com',
+  projectId: 'simonmusic-1d3ae',
+  storageBucket: 'simonmusic-1d3ae.appspot.com',
+  messagingSenderId: '243491725963'
+};
+firebase.initializeApp(config);
+
+const { FacebookAuthProvider } = firebase.auth;
+const firebaseAuth = firebase.auth();
 
 export default class Login extends Component<{}> {
   constructor(props) {
     super(props);
+
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  componentWillMount() {
+    this.authenticateUser();
+  }
+
+  authenticateUser = () => {
+    AccessToken.getCurrentAccessToken().then(
+      (data) => {
+        const { accessToken } = data;
+        const credential = FacebookAuthProvider.credential(accessToken);
+        firebaseAuth.signInWithCredential(credential).then(
+          (user) => {
+            Actions.home();
+          },
+          (error) => {
+            console.error(error);
+          }
+        )
+      }
+    )
   }
 
   handleLogin = (error, result) => {
     if (error) {
       console.error(error);
     } else if (result.isCancelled) {
-      alert("Login is cancelled");
+      alert('Login is cancelled');
     } else {
-      AccessToken.getCurrentAccessToken().then(
-        (data) => {
-          // alert(data.accessToken.toString());
-          Actions.home();
-        }
-      )
+      this.authenticateUser();
     }
   }
-
 
   render() {
     return (
@@ -36,7 +71,7 @@ export default class Login extends Component<{}> {
         <LoginButton
           readPermissions={['public_profile', 'email']}
           onLoginFinished={this.handleLogin}
-          onLogoutFinished={() => alert("Logout")} />
+          onLogoutFinished={() => alert('Logout')} />
       </View>
     );
   }
